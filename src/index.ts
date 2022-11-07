2e39f69924488e4c3899f69ddee5b49505f2ae6a
LucFERRO <luchenri.ferro@gmail.com>
2022-11-07 13:05:03 +0100

    modif

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Simplon 1'));
+app.get('/hello', (req, res) => res.send('Hello Simplon 1 LF'));
 
-app.listen(3000, ()=> console.log("Application démarrée sur le port 3000"));
+app.listen(3006, ()=> console.log("Application démarrée sur le port 3006 LF"));
