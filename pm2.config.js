728dd13971f9c14ad7c44972146d2351b9d42913
LucFERRO <luchenri.ferro@gmail.com>
2022-11-10 09:29:51 +0100

    final?

---
+++ pm2.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps: [
+        {
+            name: 'testVPS',
+            script: './dist/index.js'
+        }
+    ]
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Simplon 1 LF'));
+app.get('/hello', (req, res) => res.send('Hello Simplon LUC FERRO'));
 
 app.listen(3006, ()=> console.log("Application démarrée sur le port 3006 LF"));
