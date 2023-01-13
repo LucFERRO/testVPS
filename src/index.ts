f30a5ffd0c92b8a58aee20e6c1ae9209a2b2ab42
LucFERRO <luchenri.ferro@gmail.com>
2023-01-13 10:24:14 +0100

    update secret

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import express from "express";
 const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Simplon LUC FERRO'));
-app.get('/docker', (req, res) => res.send('Docker secret: '+ process.env.id));
+app.get('/docker', (req, res) => res.send('Docker secret updated: '+ process.env.id));
 
 app.listen(3006, ()=> console.log("Application démarrée sur le port 3006 LF"));
